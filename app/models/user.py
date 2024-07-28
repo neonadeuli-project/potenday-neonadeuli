0ead62b3293732e877535d8733a0ba88c0f10fc5
JongHyunJung <jjh19960115@gmail.com>
2024-07-29 07:28:14 +0900

    feat: User 엔티티 수정

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,29 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from enum import Enum
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from app.core.database import Base
 
+# class UserRole(enum.Enum):
+#     USER = "user"
+#     ADMIN = "admin"
+    
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
-    oauth_id = Column(String(255), unique=True, index=True)
-    email = Column(String(255), unique=True, index=True)
-    name = Column(String(20))
-    provider = Column(String(255))
+    # oauth_id = Column(String(255), unique=True, index=True, nullable=True)
+    # email = Column(String(255), unique=True, index=True)
+    name = Column(String(100))
+    # provider = Column(String(255), nullable=True)
+    # hashed_password = Column(String(255), nullable=True)
+    # is_active = Column(Boolean, default=True)
+    # role = Column(Enum(UserRole), default=UserRole.USER)
     token = Column(String(255), unique=True, index=True)
+    # refresh_token = Column(String(255), unique=True, index=True, nullable=True)
+    # refresh_token_expires_at = Column(DateTime(timezone=True), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    last_login = Column(DateTime(timezone=True), onupdate=func.now())
     
     chat_sessions = relationship("ChatSession", back_populates="users")
     bookmarks = relationship("UserBookmark", back_populates="users")
