b79df85db55ef8f934228bf6a3a3ba500c23a860
JongHyunJung <jjh19960115@gmail.com>
2024-08-01 16:58:36 +0900

    feat: 유효성 검사 service 클래스 분리

---
+++ app/service/validation_service.py
@@ -0,0 +1,23 @@
+from app.repository.chat_repository import ChatRepository
+from app.repository.heritage_repository import HeritageRepository
+from app.error.heritage_exceptions import SessionNotFoundException, BuildingNotFoundException, InvalidAssociationException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class ValidationService:
+    def __init__(self, db: AsyncSession):
+        self.chat_repository = ChatRepository(db)
+        self.heritage_repository = HeritageRepository(db)
+
+    async def validate_session_and_building(self, session_id: int, building_id: int):
+        chat_session = await self.chat_repository.get_chat_session(session_id)
+        if not chat_session:
+            raise SessionNotFoundException(session_id)
+
+        building = await self.heritage_repository.get_heritage_building_by_id(building_id)
+        if not building:
+            raise BuildingNotFoundException(building_id)
+
+        if building.heritage_id != chat_session.heritage_id:
+            raise InvalidAssociationException(session_id, building_id)
+
+        return building
