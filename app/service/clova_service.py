5028a060f775900a7a0470f64ac8918c15d5d148
JongHyunJung <jjh19960115@gmail.com>
2024-08-03 12:28:49 +0900

    refactor: Clova 퀴즈, 정보 분기 처리

--- app/service/clova_service.py
+++ app/service/clova_service.py
@@ -1,74 +1,22 @@
-from functools import lru_cache
 import logging
-import aiohttp
-from typing import Any, Callable, Dict, List, Tuple
-from app.core.config import settings
 import json
-import http.client
-from http import HTTPStatus
 import requests
+import http.client
+from typing import Dict, List
+from http import HTTPStatus
 
 from app.core.config import settings
 from app.error.chat_exception import APICallException, ChatServiceException
-from app.utils.common import parse_quiz_content
+from app.models.enums import ChatbotType
+from app.utils.prompts import *
 
-
-SYSTEM_PROMPT_CHATBOT = '''대한민국 문화재를설명하는 문화해설사입니다. 경복궁과 관련한 문화재에 대한 설명을 출력합니다.
-지시사항
--  모든 말은 '하오'체로 한다.
-- 해당 문화재와 관련한 질문이 아니라면 답하지 않는다
-- 정확한 정보만 전달한다.'''
-SYSTEM_PROMPT_QUIZ = '''1. 너는 대한민국 문화재에 대한 정확한 정보를 바탕으로 퀴즈를 내는 퀴즈 전문가입니다.
-2. 입력한 문화재와 관련한 퀴즈를 냅니다.
-3. 문제, 5개의 보기, 정답, 설명 모두를 반환합니다.
-4. 퀴즈 이외의 텍스트는 절대 표시하지 않습니다.
-5. 모든 말은 '하오'체로 합니다.
-
-### 예시
-경복궁의 중심이 되는 건물은 다음 중 무엇일까요?
-
-1. 근정전
-2. 사정전
-3. 교태전
-4. 강녕전
-5. 향원정
-
-
-정답: 1번
-
-
-설명: 근정전은 경복궁의 중심 건물로, 조선 왕조의 국왕이 공식적으로 업무를 보던 장소입니다. 중요한 의식과 국가 행사가 이곳에서 열렸으며, 경복궁의 주요 건물 중 하나로 손꼽힙니다.'''
-SYSTEM_PROMPT_ANSWER = '''너는 대한민국 문화재에 대한 정확한 정보를 바탕으로 퀴즈를 내는 퀴즈 전문가야. 내가 문제와 답을 알려주면, 정답인지 아닌지 확인하고 간결하게 설명해줘.
-지시사항
-- 모든 말은 '하오'체로 한다.
-- 정확하게 확인하고 설명한다.
-{예시}
-조선시대 임금의 즉위식이 거행되었던 경복궁의 전각은 다음 중 무엇일까요?
-1. 근정전
-2. 사정전
-3. 교태전
-4. 경회루
-5. 자경전
-###
-4번
-###
-맞소! 정답은 4번 경회루요.'''
-SYSTEM_PROMPT_SUMMARY = '''너는 대한민국 문화재를 정확하고 재미있게 설명하는 문화해설사야.
-내가 문화재를 말하면, 그 문화재에 대한 키워드를 10가지 뽑아서 보여줘. 맨 처음에는 반드시 #너나들이 가 들어가야해. {예시} #너나들이 #광화문 #경복궁 #종로 #조선시대 #궁궐 #국보 #해치 #드므 #십장생 '''
-
-SYSTEM_PROMPT_SUMMARY_TEST = '''너는 대한민국 문화재를 정확하고 재미있게 설명하는 문화해설사야.
-내가 대화한 전체 내용을 읽고 뽑을 수 있는 키워드를 10가지 뽑아서 보여줘. 맨 처음에는 반드시 #너나들이 가 들어가야해. {예시} #너나들이 #광화문 #경복궁 #종로 #조선시대 #궁궐 #국보 #해치 #드므 #십장생 '''
-
-
-MAX_TOKEN = 4000
+logger = logging.getLogger(__name__)
 
 def parse_non_stream_response(response):
     result = response.get('result', {})
     message = result.get('message', {})
     content = message.get('content', '')
     return content.strip()
-
-logger = logging.getLogger(__name__)
 
 class CLOVAStudioExecutor:
     def __init__(self, host, api_key, api_key_primary_val, request_id):
@@ -234,16 +182,10 @@ class ClovaService:
         except Exception as e:
             logger.error(f"채팅 요청 처리 중 예상치 못한 오류 발생: {str(e)}")
             raise ChatServiceException("채팅 요청 처리 중 오류 발생")
-        
-    def manage_sliding_window_size(self, sliding_window: List[Dict[str, str]]) -> List[Dict[str, str]]:
-        max_window_size = settings.MAX_SLIDING_WINDOW_SIZE
-        if len(sliding_window) > max_window_size:
-            return [sliding_window[0]] + sliding_window[-(max_window_size-1):]
-        return sliding_window
 
     # 여기서 퀴즈 버튼을 누를 때, 현재 위치의 이름을 받아와야 합니다. (ex - 근정전)
     # async def get_quiz(self, session_id: int, building_name: str) -> Dict[str, str]:
-    async def get_quiz(self, session_id: int, building_name: str) -> str:
+    async def get_info_or_quiz(self, session_id: int, building_name: str, request_type: ChatbotType) -> str:
         try:
             completion_executor = ChatCompletionExecutor(
                 host = self.api_completion_url,
@@ -251,10 +193,19 @@ class ClovaService:
                 api_key_primary_val = self.api_key_primary_val,
                 request_id = str(session_id)
             )
+
+            if request_type == "quiz":
+                system_prompt = SYSTEM_PROMPT_QUIZ
+                user_content = f"{building_name}에 대한 퀴즈를 생성해주세요."
+            elif request_type == "info":
+                system_prompt = SYSTEM_PROMPT_INFO
+                user_content = f"{building_name}에 대해 설명해주세요."
+            else:
+                raise ValueError("유효하지 않은 요청 타입입니다. 반드시 퀴즈 또는 정보 타입이어야 합니다.")
             
             request_data = [
-                {"role": "system", "content": SYSTEM_PROMPT_QUIZ}, 
-                {"role": "user", "content": f"{building_name}에 대한 퀴즈를 생성해주세요."}
+                {"role": "system", "content": system_prompt}, 
+                {"role": "user", "content": user_content}
             ]
 
             completion_request_data = {
@@ -269,7 +220,7 @@ class ClovaService:
                 "seed": 0
             }
 
-            logger.info(f"Quiz request data: {completion_request_data}")
+            logger.info(f"{request_type.capitalize()} request data: {completion_request_data}")
             response = completion_executor.execute(completion_request_data, stream=False)
             logger.info(f"Raw API response for session ID {session_id}: {response}")
             
@@ -287,69 +238,6 @@ class ClovaService:
         except Exception as e:
             logger.error(f"퀴즈 생성 중 예상치 못한 오류 발생: {str(e)}")
             raise ChatServiceException("퀴즈 생성 중 오류 발생")
-
-    # 여기서는 full_conversation에 들어있던 question과 기존 sliding_window, 
-    # 선택한 보기가 입력으로 들어와야 합니다. (ex - 1번)
-    async def get_answer(self, session_id: int, sliding_window, question: str, answer: str) -> str:
-        try:
-            sliding_window_executor = SlidingWindowExecutor(
-                    host = self.api_sliding_url,
-                    api_key = self.api_key,
-                    api_key_primary_val= self.api_key_primary_val,
-                    request_id = str(session_id)
-                )
-
-            completion_executor = ChatCompletionExecutor(
-                host = self.api_completion_url,
-                api_key = self.api_key,
-                api_key_primary_val = self.api_key_primary_val,
-                request_id = str(session_id)
-            )
-            
-            request_data = [{"role": "system", "content": SYSTEM_PROMPT_ANSWER}, {"role": "user", "content": question + "\n" + answer}]
-            completion_request_data = {
-                "messages": request_data,
-                "maxTokens": 300,
-                "temperature": 0.5,
-                "topK": 0,
-                "topP": 0.8,
-                "repeatPenalty": 1.2,
-                "stopBefore": [],
-                "includeAiFilters": True,
-                "seed": 0
-            }
-
-            response = completion_executor.execute(completion_request_data, stream=False)
-            response_text = parse_non_stream_response(response)
-            new_sliding_window = sliding_window_executor.execute(sliding_window + [{"role":"assistant", "content":response_text}])
-
-            return {"response": response_text, "new_sliding_window":new_sliding_window}
-        
-        except APICallException as e:
-            logger.error(f"답변 생성 중 API 오류 발생: {e.api_name}, 상태 코드: {e.status_code}, 오류 메시지: {e.error_message}")
-            raise ChatServiceException(f"답변 생성 중 API 오류 발생: {e.api_name}")
-        except Exception as e:
-            logger.error(f"답변 생성 중 예상치 못한 오류 발생: {str(e)}")
-            raise ChatServiceException("답변 생성 중 오류 발생")
-        
-    def manage_sliding_window_size(self, sliding_window: List[Dict[str, str]]) -> List[Dict[str, str]]:
-        max_window_size = settings.MAX_SLIDING_WINDOW_SIZE
-        if len(sliding_window) > max_window_size:
-            return [sliding_window[0]] + sliding_window[-(max_window_size-1):]
-        return sliding_window
-    
-    def update_sliding_window_system(self, sliding_window: List[Dict[str, str]], new_system_prompt: str) -> List[Dict[str, str]]:
-        # system 메시지를 새로운 프롬프트로 교체
-        updated_window = [{"role": "system", "content": new_system_prompt}]
-        
-        # 기존의 user와 assistant 메시지만 유지
-        for message in sliding_window:
-            if message['role'] in ['user', 'assistant']:
-                updated_window.append(message)
-        
-        return updated_window
-
-    
 
     # content는 돌았던 코스 텍스트가 담겨있으면 됩니다.
     async def get_summary(self, session_id: int, content: str) -> str:
@@ -388,3 +276,20 @@ class ClovaService:
         except Exception as e:
             logger.error(f"요약 생성 중 예상치 못한 오류 발생: {str(e)}")
             raise ChatServiceException("요약 생성 중 오류 발생")
+        
+    def manage_sliding_window_size(self, sliding_window: List[Dict[str, str]]) -> List[Dict[str, str]]:
+        max_window_size = settings.MAX_SLIDING_WINDOW_SIZE
+        if len(sliding_window) > max_window_size:
+            return [sliding_window[0]] + sliding_window[-(max_window_size-1):]
+        return sliding_window
+    
+    def update_sliding_window_system(self, sliding_window: List[Dict[str, str]], new_system_prompt: str) -> List[Dict[str, str]]:
+        # system 메시지를 새로운 프롬프트로 교체
+        updated_window = [{"role": "system", "content": new_system_prompt}]
+        
+        # 기존의 user와 assistant 메시지만 유지
+        for message in sliding_window:
+            if message['role'] in ['user', 'assistant']:
+                updated_window.append(message)
+        
+        return updated_window
