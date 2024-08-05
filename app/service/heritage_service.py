c6b24b5eab6944991e09658e65d4b823dfd2dc3b
JongHyunJung <jjh19960115@gmail.com>
2024-08-06 04:09:12 +0900

    feat: 문화재 상세 조회 비즈니스 로직 추가

--- app/service/heritage_service.py
+++ app/service/heritage_service.py
@@ -1,10 +1,12 @@
 import logging
 
 from typing import List
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from haversine import haversine
+from app.error.heritage_exceptions import DatabaseConnectionError, HeritageNotFoundException, InvalidCoordinatesException
 from app.repository.heritage_repository import HeritageRepository
-from app.schemas.heritage import HeritageListResponse
+from app.schemas.heritage import HeritageDetailResponse, HeritageListResponse
 from pygeodesic import geodesic
 
 logger = logging.getLogger(__name__)
@@ -13,9 +15,14 @@ class HeritageService:
     def __init__(self, db: AsyncSession):
         self.heritage_repository = HeritageRepository(db)
 
+    # 문화재 리스트 조회
     async def get_heritages(self, page: int, limit: int, user_latitude: float, user_longitude) -> List[HeritageListResponse]:
-        offset = (page - 1) * limit
-        heritages = await self.heritage_repository.search_heritages(limit, offset)
+        try:
+            offset = (page - 1) * limit
+            heritages = await self.heritage_repository.search_heritages(limit, offset)
+        except SQLAlchemyError as e:
+            logger.error(f"Database error in get_heritages: {str(e)}")
+            raise DatabaseConnectionError()
 
         user_location = (user_latitude, user_longitude)
         result = []
@@ -25,11 +32,14 @@ class HeritageService:
                     heritage_location = (float(heritage.latitude), float(heritage.longitude))
                     distance = round(haversine(user_location, heritage_location), 1)
                 else:
-                    distance = None
-                    logger.warning(f"문화재 ID가 {heritage.id}인 문화재는 위도, 경도 값이 없습니다.")
+                    raise InvalidCoordinatesException(heritage.id)
             except (ValueError, TypeError) as e:
                 distance = None
                 logger.error(f"문화재 ID가 {heritage.id}인 문화재의 거리 계산을 실패했습니다. : {str(e)}")
+                distance = None
+            except InvalidCoordinatesException as e:
+                logger.warning(str(e))
+                distance = None
                 
 
             result.append(HeritageListResponse(
@@ -40,6 +50,29 @@ class HeritageService:
                 image_url = heritage.image_url,
                 distance = distance
             ))
-            
-        return result
 
+        return result
+    
+    # 문화재 상세 조회
+    async def get_heritage_by_id(self, heritage_id: int) -> HeritageDetailResponse:
+        try:
+            heritage = await self.heritage_repository.get_heritage_by_id(heritage_id)
+            if not heritage:
+                raise HeritageNotFoundException(heritage_id)
+        except SQLAlchemyError as e:
+            logger.error(f"Database error in get_heritage_detail: {str(e)}")
+            raise DatabaseConnectionError()
+        
+        return HeritageDetailResponse (
+            id = heritage.id,
+            image_url = heritage.image_url,
+            name = heritage.name,
+            name_hanja = heritage.name_hanja,
+            description = heritage.description,
+            heritage_type = heritage.heritage_types.name if heritage.heritage_types else None,
+            category = heritage.category,
+            sub_category1 = heritage.sub_category1,
+            sub_category2 = heritage.sub_category2,
+            era = heritage.era,
+            location = heritage.location
+        )
