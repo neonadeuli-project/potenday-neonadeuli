bbd68e9ff4e6820ceeb2167fe2553477d9b8f718
JongHyunJung <jjh19960115@gmail.com>
2024-08-05 23:55:01 +0900

    feat: 이미지 처리 API 구현

--- app/router/api.py
+++ app/router/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.router.v1 import user, chat
+from app.router.v1 import user, chat, image
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
-api_router.include_router(chat.router, prefix="/heritage", tags=["heritage"])
+# api_router.include_router(heritage.router, prefix="/heritage", tags=["heritage"])
+api_router.include_router(image.router, prefix="/image", tags=["image"])
